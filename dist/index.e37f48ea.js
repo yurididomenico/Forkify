ed82d56915ba77eb33bfd142fb100d3659d9b04a
yurididomenico <yurididomenico93@gmail.com>
2024-03-26 17:15:28 +0100

    289. Loading a Recipe from API

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -594,7 +594,31 @@ const timeout = function(s) {
 };
 // https://forkify-api.herokuapp.com/v2
 ///////////////////////////////////////
-console.log("YDD");
+const showRecipe = async function() {
+    try {
+        // const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc971");
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        console.log(res, data);
+        // let recipe = data.data.recipe;
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 },{}]},["hycaY","aenu9"], "aenu9", "parcelRequire3a11")
 
